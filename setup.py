d23c3c95633cc651b238083ce28bc4f7eaac58bf
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-07-29 19:20:54 +0700

    Required package

--- setup.py
+++ setup.py
@@ -15,6 +15,9 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url="https://github.com/bervProject/Kata-Baku-Checker-for-PO-File/",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'polib',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
