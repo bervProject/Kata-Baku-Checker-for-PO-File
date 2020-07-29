eae53fb9178bd50a14c802959cd47b3ee942f75b
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-07-29 13:25:02 +0700

    Update readme

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="kata-baku-checkr-po-file",
-    version="0.1.0",
+    version="0.1.1",
     author="Bervianto Leo Pratama",
     author_email="bervianto.leo@gmail.com",
     description="Checker for Kata Baku for PO File reader",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/bervProject/Kata-Baku-Checker-for-PO-File/",
     packages=setuptools.find_packages(),
     classifiers=[
